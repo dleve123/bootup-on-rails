42a21092c0d33686b382c45880c6e3b046cd8586
Daniel Levenson <dleve123@gmail.com>
2013-01-19 18:01:03 -0500

    initial commit - includes sql db dump

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootupOnRails::Application.config.secret_token = '5157e75c63d671880c9ec4772d55ef457f3c012ef6ab842a659b4b2abbbb86b99b05ab8c28162b2d20be0cc25473a2e666d4dadbbe19b808e6b2f9d2ecd2b118'
