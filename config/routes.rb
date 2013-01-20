fc59f9639d1c8797b353482c5e3ff1a16609f866
Daniel Levenson <dleve123@gmail.com>
2013-01-20 16:10:01 -0500

    adds basic site-wide styling and adds Application#show view. Points root to Application#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 BootupOnRails::Application.routes.draw do
   resources :users
+  resource :application
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +50,7 @@ BootupOnRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'application#show'
 
   # See how all your routes lay out with "rake routes"
 
